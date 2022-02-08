1843ebf9dc6014ca041feb391ff966fbc5cea92f
Fahd Arafat <fahdarft@gmail.com>
2022-02-09 02:50:47 +0300

    Implement logic for loading more employees

--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -4,7 +4,11 @@ angular
 
 function homePageController(Employees) {
   const homePageVm = this;
+  homePageVm.currentPage = 1;
+  homePageVm.pages = 1;
   homePageVm.employees = [];
+  homePageVm.loading = false;
+  homePageVm.disabled = false;
 
   activate();
 
@@ -12,6 +16,20 @@ function homePageController(Employees) {
     Employees.getEmployees()
       .then(({ data }) => {
         homePageVm.employees = homePageVm.employees.concat(data.employees);
+        console.log(data);
       });
   }
+  homePageVm.loadMore = () => {
+    if (homePageVm.currentPage <= homePageVm.pages) {
+      homePageVm.loading = true;
+      Employees.loadMoreEmployees(homePageVm.currentPage).then(({ data }) => {
+        homePageVm.employees = homePageVm.employees.concat(data.employees);
+        homePageVm.pages = data.pages;
+        homePageVm.currentPage++;
+        homePageVm.loading = false;
+      });
+    } else {
+      homePageVm.disabled = true;
+    }
+  };
 }
