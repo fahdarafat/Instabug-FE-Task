b5ca040b2b05596784ccb89bc4ca9ed1d990863d
Fahd Arafat <fahdarft@gmail.com>
2022-02-09 18:09:32 +0300

    Adding text to the filter is now reflected in the page url as a query parameter

--- app/components/vue-components/filters.vue
+++ app/components/vue-components/filters.vue
@@ -20,11 +20,30 @@ export default {
   },
   methods: {
     clearText() {
-      (this.searchText = ""), this.$emit("clear-text");
+      this.searchText = "";
+      this.addQueryParam("search", this.searchText);
+      this.$emit("clear-text");
     },
     search() {
+      this.addQueryParam("search", this.searchText);
       this.$emit("filter-text", this.searchText);
     },
+    addQueryParam(key, value) {
+      const url = new URL(window.location.href);
+      url.searchParams.set(key, value);
+      window.history.pushState({}, "", url.toString());
+    },
+    getQueryParam(key) {
+      const url = new URL(window.location.href);
+      return url.searchParams.get(key) || "";
+    },
+  },
+  created() {
+    const query = this.getQueryParam("search");
+    if (query.length > 0) {
+      this.searchText = query;
+      this.search();
+    }
   },
 };
 </script>
